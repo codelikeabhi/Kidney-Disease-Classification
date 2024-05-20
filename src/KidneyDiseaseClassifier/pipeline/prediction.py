7ac8ae389a1f92a1c899ebe349b6d1d19c775a7b
codelikeabhi <abhisriwastav7308@gmail.com>
2024-05-20 12:33:06 +0530

    model

--- src/KidneyDiseaseClassifier/pipeline/prediction.py
+++ src/KidneyDiseaseClassifier/pipeline/prediction.py
@@ -10,4 +10,18 @@ class PredictionPipeline:
 
     def predict(self):
         #load trained model
-        model
+        model = load_model(os.path.join("model", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis = 1)
+
+        if result[0] == 1:
+            prediction = "Tumour"
+            return [{"image" : prediction}]
+
+        else:
+            prediction = "Normal"
+            return [{"image" : prediction}]
